a5ad633f246697cc6162d9b05b706034ef4a0f05
soutionscience <marshnjagi@yahoo.com>
2018-09-09 10:44:00 +0300

    added first reducesr

---
+++ src/reducer/reducer_books.js
@@ -0,0 +1,9 @@
+export default function (){
+    return[
+        {title: 'javascript the good parts'},
+        {title: 'the art of igonring calls'},
+        {title: 'mambo yote'},
+        {title: 'The vote'},
+        {title: 'why always me'}
+    ];
+}
